2c15715e4b63883c103e6c569e477a177b3bf4e4
julas23 <julianoas@msn.com>
2023-08-24 11:38:31 +0100

    improved backup script

--- backup.sh
+++ backup.sh
@@ -11,6 +11,22 @@ display_usage() {
     exit 1
 }
 
+# Function to check if a directory is mounted
+is_mounted() {
+    mountpoint -q "$1"
+}
+
+# Function to attempt mounting a directory
+attempt_mount() {
+    if ! is_mounted "$1"; then
+        echo "Mounting '$1'..."
+        if ! mount "$1"; then
+            echo "Failed to mount '$1'. Aborting."
+            exit 1
+        fi
+    fi
+}
+
 # Process command-line arguments
 while [[ $# -gt 0 ]]; do
     key="$1"
@@ -46,6 +62,9 @@ if [ ! -d "$destination_directory" ]; then
     echo "Destination directory '$destination_directory' does not exist."
     exit 1
 fi
+
+# Attempt to mount destination directory if it's not mounted
+attempt_mount "$destination_directory"
 
 # Compare files between source and destination directories
 diff_output=$(rsync -rcv --delete "$source_directory/" "$destination_directory/")
